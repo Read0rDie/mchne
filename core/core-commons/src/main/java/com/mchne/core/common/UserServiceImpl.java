60c509a404fd52629f1a8d13eb5bb05bde3eac25
Alex Dubuisson <gherfrost@gmail.com>
2019-06-30 03:15:09 -0400

    restructuring

--- core/core-commons/src/main/java/com/mchne/core/common/UserServiceImpl.java
+++ core/core-commons/src/main/java/com/mchne/core/common/UserServiceImpl.java
@@ -1,5 +1,22 @@
 package com.mchne.core.common;
 
 
-public class UserServiceImpl {
+import com.mchne.core.data.persistence.entity.User;
+import com.mchne.core.data.persistence.repository.UserRepository;
+import com.mchne.core.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
 }
--- core/core-services/src/main/java/com/mchne/core/services/UserService.java
+++ core/core-services/src/main/java/com/mchne/core/services/UserService.java
@@ -1,4 +1,14 @@
 package com.mchne.core.services;
 
+import org.springframework.stereotype.Service;
+import com.mchne.core.data.persistence.entity.User;
+
+
+import java.util.List;
+
+@Service
 public interface UserService {
+
+    List<User> list();
+
 }
